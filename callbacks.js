cbf84f240b65648a85abbea9f59556c5365d6669
Rajiv Shankar <rajiv.shankar.email@gmail.com>
2018-06-26 09:10:22 -0400

    backup

--- callbacks.js
+++ callbacks.js
@@ -1,3 +1,19 @@
+// mimic how to fetch from a server, getting them & creating a blog post
+
+// array w 2 post objects
 const posts = [
   { title: "Post One", body: "This is Post One" },
+  { title: "Post Two", body: "This is Post Two" }
 ];
+
+// get the posts and put them on the page
+function getPosts() {
+  setTimeout(() => {
+    // setTimeout: to delay getting result by 1 sec (mimic a server)
+    let output = "";
+    // forEach: method executes a provided function once for each array element
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+  }, 1000);
+}
